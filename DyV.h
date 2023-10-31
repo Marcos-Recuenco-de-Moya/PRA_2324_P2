5a2888d869de5e27f06f73ddf968e2aa347389e5
MRECDEM <mrecdem@alumno.upv.es@ldsic-vdi20.upvnet.upv.es>
2023-10-31 18:56:02 +0100

    subiendo todo

--- DyV.h
+++ DyV.h
@@ -7,7 +7,6 @@ template <typename T> //con el template puede pasarte cualquier tipo de datos, s
 
 int BusquedaBinaria(T X, vector<T>& V, int ini, int fin){
 	if(ini>fin){
-		cout << "No se encuentra en el array";
 		return -1;
 	}
 	int medio = (ini + fin) / 2;
@@ -19,3 +18,69 @@ int BusquedaBinaria(T X, vector<T>& V, int ini, int fin){
 		return BusquedaBinaria(X, V, medio +1, fin);
 	}
 }
+
+
+template <typename T>
+
+int BusquedaBinariaINV(T X, vector<T>& V, int ini, int fin){
+        if(ini>fin){
+                return -1;
+        }
+        int medio = (ini + fin) / 2;
+        if(V[medio] == X){
+                return medio;
+        }else if(V[medio] > X){
+                return BusquedaBinaria(X, V, medio+1, fin);
+        }else{
+                return BusquedaBinaria(X, V, ini, medio-1);
+        }
+}
+
+
+template <typename T>
+
+int Partition(vector<T>& V, int ini, int fin){
+	T x = V[fin];
+	int i = ini;
+	for(int j=ini ; j<fin-1 ; i++){
+		if( V[j] <= x){
+			T aux = V[i];
+			V[j] = V[i];
+			V[i] = aux;
+		}
+		T aux2 = V[i];
+		V[fin] = V[i];
+		V[i] = aux2;
+	}
+	return i;
+}
+
+
+template <typename T>
+
+void QuickSort(vector<T>& V, int ini, int fin){
+
+	if(ini < fin){
+		int pivot = Partition(V, ini, fin);
+		QuickSort(V, ini, pivot - 1);
+		QuickSort(V, pivot + 1, fin);
+	}	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ testDyV.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include "DyV.h"
+
+using namespace std;
+
+int main(){
+	
+	cout << "************BÚSQUEDA BINARIA************" << endl << endl;
+
+        vector<int> Vint{1,2,3,4,5,6,7,8,9};
+	cout << "Búsqueda del número 7 en posición: " << BusquedaBinaria(7,Vint,0,8) << endl;
+        cout << "Búsqueda del número -3 en posición: " << BusquedaBinaria(-3, Vint, 0, 8) << endl;
+
+        vector<char> Vchar{'a','b','c','d','e','f','g','h','i'};
+	cout << "Búsqueda del carácter 'c' en posición: " << BusquedaBinaria('c',Vchar,0,8) << endl;
+        cout << "Búsqueda del carácter 'z' en posición: " << BusquedaBinaria('z', Vchar, 0, 8) << endl;
+
+        vector<double> Vdouble{1.2,2.6,3.9,4.1,5.0,6.9,7.3,8.4,9.7};
+	cout << "Búsqueda del número 9.7 en posición: " << BusquedaBinaria(9.7,Vdouble,0,8) << endl;
+        cout << "Búsqueda del número 3.6 en posición: " << BusquedaBinaria(3.6, Vdouble, 0, 8) << endl;
+
+
+	cout << endl;
+	cout << "************BÚSQUEDA BINARIA INVERSA************" << endl << endl;
+
+	vector<int> VintINV{9,8,7,6,5,4,3,2,1};
+        cout << "Búsqueda del número 5 en posición: " << BusquedaBinariaINV(5,VintINV,0,8) << endl;
+        cout << "Búsqueda del número -3 en posición: " << BusquedaBinariaINV(-3, VintINV, 0, 8) << endl;
+
+        vector<char> VcharINV{'i','h','g','f','e','d','c','b','a'};
+        cout << "Búsqueda del carácter 'c' en posición: " << BusquedaBinariaINV('c',VcharINV,0,8) << endl;
+        cout << "Búsqueda del carácter 'z' en posición: " << BusquedaBinariaINV('z', VcharINV, 0, 8) << endl;
+
+        vector<double> VdoubleINV{9.7,8.4,7.3,6.9,5.0,4.1,3.9,2.6,1.2};
+        cout << "Búsqueda del número 8.4 en posición: " << BusquedaBinariaINV(8.4,VdoubleINV,0,8) << endl;
+        cout << "Búsqueda del número 3.6 en posición: " << BusquedaBinariaINV(3.6, VdoubleINV, 0, 8) << endl;
+
+
+	cout << endl;
+	cout << "************QUICK SORT************" << endl << endl;
+	vector<int> Vintquick{2,8,3,0,4,2,7,1,5,8,6};
+	QuickSort(Vintquick,0,10);
+	for(int i=0 ; i<11 ; i++){
+		cout << "[" << Vintquick[i] << ", ";
+	}
+	cout << "]" << endl;
+
+
+	vector<char> Vcharquick{'c','g','i','f','d','a','h','e','b'};
+        QuickSort(Vcharquick,0,10);
+        for(int i=0 ; i<11 ; i++){
+                cout << "[" << Vcharquick[i] << ", ";
+        }
+        cout << "]" << endl;
+
+	vector<double> Vdoublequick{7.3,2.6,9.7,1.2,4.1,5.0,6.9,8.4,3.9};
+        QuickSort(Vdoublequick,0,10);
+        for(int i=0 ; i<11 ; i++){
+                cout << "[" << Vdoublequick[i] << ", ";
+        }
+        cout << "]" << endl;
+
+
+
+
+
+
+
+     return 0;
+}
+
+
